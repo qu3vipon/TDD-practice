c84b2378acec053d62529a6c74e3563e5594154c
qu3vipon <sdh5813@gmail.com>
2020-05-31 00:02:27 +0900

    refactor: template을 이용한 구현 결과물 테스트

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@ import json
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 with open('secrets.json') as f:
     SECRETS = json.load(f)
@@ -19,6 +20,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
         self.fail('Finish the test!')
 
 
