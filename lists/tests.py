a54e9a05b899209f9bb606e3aa7e4e6b6dff7304
qu3vipon <sdh5813@gmail.com>
2020-06-01 08:45:27 +0900

    feat: Item 모델 생성 및 기본 테스트

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,17 @@
+import re
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 
+from .models import Item
 from .views import home_page
 
 
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -22,7 +27,11 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -36,4 +45,26 @@ class HomePageTest(TestCase):
             'home.html',
             {'new_item_text': '신규 작업 아이템'}
         )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
