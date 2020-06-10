3370f088d5fd0c698d83b8b1cc43f8248f8a8cd1
qu3vipon <sdh5813@gmail.com>
2020-06-11 04:48:08 +0900

    feat: 기존 리스트에 아이템을 추가하기 위한 신규 URL 및 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -96,3 +96,36 @@ class NewListTest(TestCase):
         )
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
