b077b7065eabb319f4f0d4ab53201233f4e913f8
qu3vipon <sdh5813@gmail.com>
2020-06-11 13:49:44 +0900

    feat: list 앱 URL 분리

--- lists/urls.py
+++ lists/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from lists.views import home_page
+from .views import view_list, new_list, add_item
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_page, name='home'),
-    path('lists/', include('lists.urls')),
+    path('<int:list_id>/', view_list, name='view_list'),
+    path('<int:list_id>/add_item/', add_item, name='add_item'),
+    path('new/', new_list, name='new_list'),
 ]
