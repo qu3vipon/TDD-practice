41a81b345c1cede1028e644ddbfd78ef8bd9fda5
qu3vipon <sdh5813@gmail.com>
2020-06-12 16:09:29 +0900

    refactor: 하나의 url에서 신규 아이템 POST도 처리하도록 수정

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -49,6 +49,37 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, '다른 목록 아이템 1')
         self.assertNotContains(response, '다른 목록 아이템 2')
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item/',
+            data={'item_text': '기존 목록에 신규 아이템'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
@@ -68,35 +99,14 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_bakc_to_home_page_template(self):
+        response = self.client.post('/lists/new/', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = "빈 아이템을 등록할 수 없습니다."
+        self.assertContains(response, expected_error)
 
-class NewItemTest(TestCase):
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            f'/lists/{correct_list.id}/add_item/',
-            data={'item_text': '기존 목록에 신규 아이템'}
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, '기존 목록에 신규 아이템')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f'/lists/{correct_list.id}/add_item/',
-            data={'item_text': '기존 목록에 신규 아이템'}
-        )
-
-        self.assertRedirects(response, f'/lists/{correct_list.id}/')
-
-    def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
-        self.assertEqual(response.context['list'], correct_list)
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new/', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
--- lists/urls.py
+++ lists/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import view_list, new_list, add_item
+from .views import view_list, new_list
 
 urlpatterns = [
     path('<int:list_id>/', view_list, name='view_list'),
-    path('<int:list_id>/add_item/', add_item, name='add_item'),
     path('new/', new_list, name='new_list'),
 ]
